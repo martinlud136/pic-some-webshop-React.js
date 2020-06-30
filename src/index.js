f71e75b1f08d693ca79afa17529996121319752c
Martinlud <hectormartinluduena@gmail.com>
2020-06-30 16:34:53 -0300

    setup context

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import {BrowserRouter as Router} from 'react-router-dom'
+import {ContextProvider} from './Context'
 
 ReactDOM.render(
   <React.StrictMode>
-     <Router>
+    <ContextProvider>
+      <Router>
        <App />
      </Router>
+    </ContextProvider> 
   </React.StrictMode>,
   document.getElementById('root')
 );
